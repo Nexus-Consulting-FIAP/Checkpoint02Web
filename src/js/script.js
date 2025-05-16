1bf6c9e12dcc71f2aebdafda735e85cf21c2d80e
João Pedro <jp.marcilio2@gmail.com>
2025-05-16 11:27:58 -0300

    INDEX.HTML: ajustando responsividade

--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,27 @@
 //Hide|Show Header ======
 const nav = document.querySelector("nav");
 
+var isUp = true;
 var isOver;
 nav.addEventListener("mouseenter", () => {
+    isOver = true;
     nav.style.opacity = "1";
 })
 
 nav.addEventListener("mouseleave", () => {
-    nav.style.opacity = "0";
+    isOver = false;
+    if (!isUp){        
+        nav.style.opacity = "0";
+    }
 })
 window.onscroll = function() {
     currentScrollPos = window.pageYOffset;
     if (50 > currentScrollPos) {
       nav.style.opacity = "1";
+      isUp = true;
     } else if (50 <= currentScrollPos && !isOver) {
       nav.style.opacity = "0";
+      isUp = false;
     }
 }
 
